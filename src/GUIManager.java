4cd31244f2bb26f9d61d71fc79f95d5517bf6163
Snubb <60389050+Snubb@users.noreply.github.com>
2021-02-19 11:16:25 +0100

    Man kan öppna dokument men spara e svårt.

--- src/GUIManager.java
+++ src/GUIManager.java
@@ -16,20 +16,53 @@ public class GUIManager {
             @Override
             public void actionPerformed(ActionEvent e) {
                 textArea1.append("\n" + textArea1.getText());
-                //System.out.println(textArea1.getText());
+                System.out.println(textArea1.getText());
             }
         });
         saveButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setDialogTitle("Specify a file to save");
+            public void actionPerformed(ActionEvent a) {
+                // Öppna fil för läsning
+                JFileChooser fc = new JFileChooser();
+                int result = fc.showOpenDialog(null);
+                String filename;
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    filename = fc.getSelectedFile().getAbsolutePath();
+                } else {
+                    filename = "exempel.txt";
+                }
 
-                int userSelection = fileChooser.showSaveDialog(panel1);
+                FileReader fr = null;
+                try {
+                    fr = new FileReader(filename);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                BufferedReader inFile = new BufferedReader(fr);
 
-                if (userSelection == JFileChooser.APPROVE_OPTION) {
-                    File fileToSave = fileChooser.getSelectedFile();
-                    System.out.println("Save as file: " + fileToSave.getAbsolutePath());
+                //Öppna fil för skrivning
+                String filename2 = filename+"Copy";
+                FileWriter fw = null;
+                try {
+                    fw = new FileWriter(filename2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter outFile = new PrintWriter(bw);
+
+                // Läs in filen
+                String line;
+                try {
+                    for (int i = 0; i < textArea1.toString().length(); i++) {
+                        outFile.println(textArea1);
+                    }
+
+                    inFile.close();
+                    outFile.flush();
+                    outFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -54,26 +87,14 @@ public class GUIManager {
                 }
                 BufferedReader inFile = new BufferedReader(fr);
 
-                //Öppna fil för skrivning
-                String filename2 = filename+"Copy";
-                FileWriter fw = null;
-                try {
-                    fw = new FileWriter(filename2);
-                } catch (IOException a) {
-                    a.printStackTrace();
-                }
-                BufferedWriter bw = new BufferedWriter(fw);
-                PrintWriter outFile = new PrintWriter(bw);
-
                 // Läs in filen
                 String line;
                 try {
                     while ((line = inFile.readLine() ) != null) {
-                        outFile.println(line);
+                        textArea1.setText("");
+                        textArea1.append(line);
                     }
                     inFile.close();
-                    outFile.flush();
-                    outFile.close();
                 } catch (IOException a) {
                     a.printStackTrace();
                 }
